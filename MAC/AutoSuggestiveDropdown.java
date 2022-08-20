c097feab1c2bb4845e0c95ba8e2eb20c3adbc676
Micky <62682451+mickybelarmino@users.noreply.github.com>
2022-08-20 23:55:50 +0800

    Add AutoSuggestiveDropdown.java
    Add AutoSuggestiveDropdown.java

---
+++ MAC/AutoSuggestiveDropdown.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AutoSuggestiveDropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "/Users/mickybelarmino/Documents/EclipseWorkspace/browserdrivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		driver.findElement(By.id("autosuggest")).sendKeys("ind");
+		Thread.sleep(3000);
+		
+		List <WebElement>options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a")); 
+		for(WebElement option : options) {
+			if(option.getText().equalsIgnoreCase("India")) {
+				option.click();
+				break;
+			}
+		}
+		
+		/*WebElement source=driver.findElement(By.id("hp-widget__sfrom"));
+		source.clear();
+		source.sendKeys("MUM");
+		Thread.sleep(2000);
+		source.sendKeys(Keys.ENTER);
+		
+		WebElement destination=driver.findElement(By.id("hp-widget__sTo"));
+		destination.clear();
+		destination.sendKeys("DEL");
+		Thread.sleep(2000);
+		destination.sendKeys(Keys.ARROW_DOWN);
+		destination.sendKeys(Keys.ENTER);
+		*/
+
+	}
+
+}
